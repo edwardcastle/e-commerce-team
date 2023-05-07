0d37e8cb1050c5057688048f9f693937b8e2ed1f
edwardcastle <eduardo.castillo@teavaro.com>
2023-05-07 17:50:39 -0400

    update branch

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
-<script setup>
-import { ref } from 'vue'
-
-const result = ref(null)
-fetch('https://fakestoreapi.com/products/category/women\'s clothing')
-  .then((res) => res.json())
-  .then((json) => (result.value = json))
-</script>
+<script setup></script>
 
 <template>
   <div>
     <RouterView />
-    {{ result }}
   </div>
 </template>
 
