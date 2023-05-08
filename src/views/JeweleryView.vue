5e85fe4522b23184f5ded11389d52a69266532ff
Demoad <adriprev95@gmail.com>
2023-05-09 00:40:03 +0200

    main view jewelery and carrito.svg

--- src/views/JeweleryView.vue
+++ src/views/JeweleryView.vue
@@ -1,7 +1,44 @@
-<template>
-  <div class="bg-slate-600 w-64 mx-auto">algo</div>
-</template>
+<script setup>
+import { ref } from 'vue';
+const productos = ref(null);
 
-<script setup></script>
+fetch('https://fakestoreapi.com/products/category/jewelery').then(
+  async (res) => (productos.value = await res.json())
+);
+console.log(productos);
+</script>
+
+<template>
+  <header>
+    <div class="flex flex-wrap justify-center content-center flex-col w-screen h-36">
+      <span class="text-center font-sans text-4xl font-bold">Jewelery</span>
+      <span class="text-center font-sans font-semibold">Productos destacados</span>
+    </div>
+  </header>
+
+  <section>
+    <div v-if="productos">
+      <div class="flex flex-wrap justify-center content-center gap-7 my-4 xl:mx-44">
+        <div v-for="product in productos" :key="product.id" class="shadow-lg rounded-xl">
+          <img class="w-64 h-64 mx-auto" :src="product.image" />
+          <div class="grid grid-cols-3 gap-1 my-3">
+            <div class="col-span-2 flex flex-col mx-3">
+              <span class="truncate w-44 font-bold" :title="product.title">
+                {{ product.title }}</span>
+              <span class="text-slate-400 font-semibold">{{
+                `${product.price}$`
+              }}</span>
+            </div>
+            <div class="col-span-1 flex flex-wrap justify-center content-center">
+              <button>
+                <img class="w-8 h-8 hover:scale-110 hover:" src="@/assets/icon/carrito.svg" alt="" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
 
 <style scoped></style>
