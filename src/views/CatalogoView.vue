9871553c93a85fd6d03d1bcd85eaf15ade3b0132
Demoad <adriprev95@gmail.com>
2023-05-21 21:38:14 +0200

    fix: Responsive design

--- src/views/CatalogoView.vue
+++ src/views/CatalogoView.vue
@@ -1,21 +1,23 @@
 <template>
   <header class="bg-slate-100 border-2">
-    <div class="flex flex-wrap h-96 my-4 xl:mx-52 lg:mx-36 md:mx-20">
+    <div
+      class="grid grid-cols-1 justify-center sm:flex sm:flex-wrap h-full sm:h-96 my-4 xl:mx-52 lg:mx-36 md:mx-20"
+    >
       <div
         class="xl:w-2/3 lg:w-2/3 md:w-1/2 sm:w-1/2 grid grid-cols-1 content-center"
       >
-        <span class="font-sans text-4xl font-bold"
+        <span class="font-sans text-4xl font-bold text-center sm:text-left"
           >Categorías <br />
           destacadas</span
         >
         <br />
-        <span class="font-sans font-normal"
+        <span class="font-sans font-normal text-center sm:text-left"
           >Encuentra lo que estas buscando en <br />
           nuestras categorías</span
         >
       </div>
       <div
-        class="xl:w-1/3 lg:w-1/3 md:w-1/2 sm:w-1/2 grid grid-cols-2 col-span-1 sm:gap-7 md:gap-9 lg:gap-10 xl:gap-11"
+        class="xl:w-1/3 lg:w-1/3 md:w-1/2 sm:w-1/2 grid grid-cols-2 mx-auto col-span-1 pt-3 sm:pt-0 gap-5 sm:gap-7 md:gap-9 lg:gap-10 xl:gap-11"
       >
         <div class="col-span-1 w-40 h-40">
           <img
@@ -68,7 +70,7 @@
   <section>
     <div v-if="productos">
       <div
-        class="flex flex-wrap justify-center content-center gap-7 my-4 xl:mx-44"
+        class="flex flex-wrap justify-center content-center gap-7 my-4 sm:mx-4 md:mx-20 lg:mx-36 xl:mx-44"
       >
         <div v-for="product in productos" :key="product.id" class="rounded-xl">
           <img class="w-64 h-64 mx-auto" :src="product.image" />
@@ -108,6 +110,7 @@ const router = useRouter();
 const productos = ref(null);
 fetch(`https://fakestoreapi.com/products`).then(async (res) => {
   productos.value = await res.json();
+  console.log(productos);
 });
 </script>
 
